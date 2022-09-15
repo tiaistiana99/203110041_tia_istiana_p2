35fe96671586f8f10d17c3c61fe6957a0690f2e1
intanjayadi <tiaistiana2001@gmail.com>
2022-09-15 10:46:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a203110041_tia_istiana_p2/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.a203110041_tia_istiana_p2
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.io.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fileName = findViewById<EditText>(R.id.editFile)
+        val fileData = findViewById<EditText>(R.id.editData)
+
+        val btnSave = findViewById<Button>(R.id.btnSave)
+        val btnView = findViewById<Button>(R.id.btnView)
+
+        btnSave.setOnClickListener(View.OnClickListener {
+            val file:String = fileName.text.toString()
+            val data:String = fileData.text.toString()
+            val fileOutputStream:FileOutputStream
+            try {
+                fileOutputStream = openFileOutput(file, Context.MODE_PRIVATE)
+                fileOutputStream.write(data.toByteArray())
+            } catch (e: FileNotFoundException){
+                e.printStackTrace()
+            }catch (e: NumberFormatException){
+                e.printStackTrace()
+            }catch (e: IOException){
+                e.printStackTrace()
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+            Toast.makeText(applicationContext,"data save",Toast.LENGTH_LONG).show()
+            fileName.text.clear()
+            fileData.text.clear()
+        })
+
+        btnView.setOnClickListener(View.OnClickListener {
+            val filename = fileName.text.toString()
+            if(filename.toString()!=null && filename.toString().trim()!=""){
+                var fileInputStream: FileInputStream? = null
+                fileInputStream = openFileInput(filename)
+                var inputStreamReader: InputStreamReader = InputStreamReader(fileInputStream)
+                val bufferedReader: BufferedReader = BufferedReader(inputStreamReader)
+                val stringBuilder: StringBuilder = StringBuilder()
+                var text: String? = null
+                while ({ text = bufferedReader.readLine(); text }() != null) {
+                    stringBuilder.append(text)
+                }
+                //Displaying data on EditText
+                fileData.setText(stringBuilder.toString()).toString()
+            }else{
+                Toast.makeText(applicationContext,"file name cannot be blank",Toast.LENGTH_LONG).show()
+            }
+        })
+
+    }
+}
